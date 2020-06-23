24ba65fbf3134a0cfb122c87c745e2bf9183c7ba
Liyi <xmage211120@gmail.com>
2020-06-24 05:13:27 +0800

    adjust store.js,let redux could use Local Storage to store previous state,and retrieve previous state from Local Storage at beginning,and then  recover previous state.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,38 @@
 import { createStore, applyMiddleware } from "redux";
-import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 import logger from "redux-logger";
 
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (!serializedState) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+}
+const persistedState = loadFromLocalStorage();
 const middleware = [logger];
-export const store = createStore(rootReducer, applyMiddleware(...middleware));
-export const persistor = persistStore(store);
+
+const store = createStore(
+  rootReducer,
+  persistedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// 只要store有更動 就會執行裡頭的callback function
+store.subscribe(() => {
+  saveToLocalStorage(store.getState());
+});
 
 export default store;
